ba19057cb1232ef889f0822964d395c25087e128
Benjamin Stammen <benjamin@stammen.me>
2015-11-14 17:05:19 -0500

    main screen now has table UI

--- core/src/com/blocparty/game/screens/MenuScreen.java
+++ core/src/com/blocparty/game/screens/MenuScreen.java
@@ -1,75 +1,83 @@
 package com.blocparty.game.screens;
 
-
-
-import java.util.LinkedList;
-import java.util.List;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.blocparty.game.BlocParty;
 
 public class MenuScreen implements Screen {
 	
-	Stage stage;
-	Skin skin;
-	
-	
-	
-	public MenuScreen() {
-		makeItFit();
-	}
-	
+	private Stage stage;
+	private Skin skin;
+	private Table table;
+
+	private TextButton playButton;
+	private TextButton quitButton;
+
 	@Override
-	public void resize(int width, int height) {
-		makeItFit();
-	}
-	
-	@Override
-	public void render(float arg0) {
+	public void render(float delta) {
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+
+		stage.act(delta);
 		stage.draw();
 	}
+
+	@Override
+	public void resize(int width, int height) {
+		createGUI();
+	}
+
+	@Override
+	public void show() {
+		createGUI();
+	}
 	
-	private void makeItFit() {
+	private void createGUI() {
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
 		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
-		
-		
-		
-		
-		TextButton levelScreen = new TextButton("Game Play Screen", skin);
-		//x, y, width, height
-		levelScreen.setBounds(100, 100, 200, 40);
-		stage.addActor(levelScreen);
-		levelScreen.addListener(new ChangeListener() {
+
+//		TextButton levelScreen = new TextButton("Game Play Screen", skin);
+//		//x, y, width, height
+//		levelScreen.setBounds(100, 100, 200, 40);
+//		stage.addActor(levelScreen);
+
+
+		table = new Table();
+		table.setWidth(stage.getWidth());
+		table.align(Align.center | Align.top);
+		table.setPosition(0, Gdx.graphics.getHeight());
+
+		playButton = new TextButton("Play", skin);
+		playButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
 				BlocParty.getInstance().setScreen(new GameplayScreen());
 			}
 		});
-		
-		
+
+		quitButton = new TextButton("Exit", skin);
+		quitButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.exit();
+			}
+		});
+
+		table.padTop(50);
+		table.add(playButton).padBottom(50);
+		table.row();
+		table.add(quitButton);
+
+		stage.addActor(table);
 	}
 	
 	
@@ -78,17 +86,17 @@ public class MenuScreen implements Screen {
 	
 	@Override
 	public void dispose() {
+		stage.dispose();
+		skin.dispose();
 	}
 	@Override
 	public void hide() {
+		dispose();
 	}
 	@Override
 	public void pause() {
 	}
 	@Override
 	public void resume() {
-	}
-	@Override
-	public void show() {
 	}
 }
---
+++ core/src/com/blocparty/game/utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.blocparty.game.utilities;
+
+/**
+ * Created by sfotm on 11/14/15.
+ */
+public class Constants {
+    public static final String TITLE = "BlocParty";
+}
