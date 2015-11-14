30fd3fde2255e7991fc8461c16ce9028474e2e39
Michael Abbott <abbottm07@gmail.com>
2015-11-14 14:02:13 -0500

    Box clicking -- 2

--- core/src/com/blocparty/game/screens/GameplayScreen.java
+++ core/src/com/blocparty/game/screens/GameplayScreen.java
@@ -2,33 +2,22 @@ package com.blocparty.game.screens;
 
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.blocparty.game.utilities.GameClickListener;
+
 
 public class GameplayScreen implements Screen {
 
@@ -43,8 +32,19 @@ public class GameplayScreen implements Screen {
 
     private int width;
     private int height;
-    private int boxWidth;
-    private int boxHeight;
+    public static int boxWidth;
+    public static int boxHeight;
+    
+    public static ArrayList<Box> boxes;
+    //Box[][] thing;
+    
+    public class Box {
+    	public int row;
+    	public int column;
+    	
+    	public int circleRadius = 20;
+    }
+    
 
 
     public GameplayScreen() {
@@ -67,22 +67,14 @@ public class GameplayScreen implements Screen {
         shapeBatch.setColor(Color.BLACK);
         //shapeBatch.rect(0, 0, 100, 50);
 
-        int xDist = 0;
-        int yDist = 0;
-        int count = 0;
-        while (xDist < width) {
-            //x, y, x2, y2
-            //shapeBatch.line(xDist, yDist, );
-            yDist = 0;
-            while (yDist < height) {
-                //shapeBatch.line(xDist, yDist, xDist, yDist + boxHeight);
-                shapeBatch.rect(xDist, yDist, boxWidth, boxHeight);
-                yDist += boxHeight;
-                count++;
-            }
-            xDist += boxWidth;
+        
+        
+        for (int i = 0; i < 4; i++) {
+        	for (int j = 0; j < 2; j++) {
+        		shapeBatch.rect(i * boxWidth, j * boxHeight, boxWidth, boxHeight);
+        	}
         }
-        System.out.println(count);
+        
 
 
         shapeBatch.end();
@@ -107,6 +99,21 @@ public class GameplayScreen implements Screen {
 
         boxWidth = width / 4;
         boxHeight = height / 2;
+        
+        
+        
+        
+        boxes = new ArrayList<Box>();
+        for (int i = 0; i < 4; i++) {
+        	for (int j = 0; j < 2; j++) {
+        		Box b = new Box();
+        		b.row = j;
+        		b.column = i;
+        		boxes.add(b);
+        	}
+        }
+        
+        
 
 
 
@@ -144,4 +151,3 @@ public class GameplayScreen implements Screen {
     public void show() {
     }
 }
-
