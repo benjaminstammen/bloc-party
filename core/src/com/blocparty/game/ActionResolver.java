05e693c10497bcc4b7f6a4b9ff49954db9138d5d
Joshua Kahn <j.harris.kahn@gmail.com>
2015-11-14 12:58:46 -0500

    Added ActionResolver interface for Google Play integration.

---
+++ core/src/com/blocparty/game/ActionResolver.java
@@ -0,0 +1,15 @@
+package com.blocparty.game;
+
+/**
+ * Created by jkahn on 11/14/15.
+ */
+public interface ActionResolver {
+
+    public boolean getSignedInGPGS();
+    public void loginGPGS();
+    public void submitScoreGPGS(int score);
+    public void unlockAchievementGPGS(String achievementId);
+    public void getLeaderboardGPGS();
+    public void getAchievementsGPGS();
+
+}
