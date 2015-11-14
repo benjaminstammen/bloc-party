ad887b85a31e760321086ecac0e281f3176953d1
Joshua Kahn <j.harris.kahn@gmail.com>
2015-11-14 14:25:19 -0500

    Fixing ActionResolver.

--- android/src/com/blocparty/game/android/AndroidLauncher.java
+++ android/src/com/blocparty/game/android/AndroidLauncher.java
@@ -1,29 +1,67 @@
 package com.blocparty.game.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.blocparty.game.ActionResolver;
 import com.blocparty.game.BlocParty;
+import com.google.example.games.basegameutils.GameHelper;
 
-public class AndroidLauncher extends AndroidApplication implements ActionResolver {//, GameHelperListener {
+public class AndroidLauncher extends AndroidApplication implements ActionResolver, GameHelper.GameHelperListener {
+
+	private GameHelper gameHelper;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		BlocParty blocParty = BlocParty.getInstance(this);
 		initialize(blocParty, config);
+
+		if (gameHelper == null) {
+			gameHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
+			gameHelper.enableDebugLog(true);
+		}
+		gameHelper.setup(this);
+	}
+
+	@Override
+	public void onStart(){
+		super.onStart();
+		gameHelper.onStart(this);
+	}
+
+	@Override
+	public void onStop(){
+		super.onStop();
+		gameHelper.onStop();
+	}
+
+	@Override
+	public void onActivityResult(int request, int response, Intent data) {
+		super.onActivityResult(request, response, data);
+		gameHelper.onActivityResult(request, response, data);
 	}
 
 	@Override
 	public boolean getSignedInGPGS() {
-		return false;
+		return gameHelper.isSignedIn();
 	}
 
 	@Override
 	public void loginGPGS() {
-
+		try {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					gameHelper.beginUserInitiatedSignIn();
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -38,11 +76,25 @@ public class AndroidLauncher extends AndroidApplication implements ActionResolve
 
 	@Override
 	public void getLeaderboardGPGS() {
-
+		if (gameHelper.isSignedIn())
+		{
+		} else if (!gameHelper.isConnecting()) {
+			loginGPGS();
+		}
 	}
 
 	@Override
 	public void getAchievementsGPGS() {
+
+	}
+
+	@Override
+	public void onSignInFailed() {
+
+	}
+
+	@Override
+	public void onSignInSucceeded() {
 
 	}
 }
--- core/src/com/blocparty/game/BlocParty.java
+++ core/src/com/blocparty/game/BlocParty.java
@@ -3,21 +3,30 @@ package com.blocparty.game;
 import com.badlogic.gdx.Game;
 import com.blocparty.game.screens.MenuScreen;
 
+import javax.swing.Action;
+
 //a singleton, controller for the screens, and will possibly be a container for variables shared across screens...
 //although variables shared across screens could possibly be a singleton as well
 public class BlocParty extends Game {
 
 
 	private static BlocParty GAME = null;
+	private ActionResolver actionResolver;
 
-	private BlocParty(/*ActionResolver resolver*/) {
+	private BlocParty(ActionResolver resolver) {
+		actionResolver = resolver;
 		//singleton
 	}
 
-	public static BlocParty getInstance(/*ActionResolver resolver*/) {
+	public static BlocParty getInstance(ActionResolver resolver) {
 		if (GAME == null) {
-			GAME = new BlocParty(/*resolver*/);
+			GAME = new BlocParty(resolver);
 		}
+		return GAME;
+	}
+
+	public static BlocParty getInstance() {
+
 		return GAME;
 	}
 
