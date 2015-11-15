ec23984f64e401e6f52e9c20baf18267851d8ba2
Benjamin Stammen <benjamin@stammen.me>
2015-11-15 02:32:52 -0500

    made circles objects

--- core/src/com/blocparty/game/screens/GameplayScreen.java
+++ core/src/com/blocparty/game/screens/GameplayScreen.java
@@ -1,12 +1,11 @@
 package com.blocparty.game.screens;
 
-
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -14,14 +13,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.blocparty.game.BlocParty;
+import com.blocparty.game.actors.ExpandingTile;
 import com.blocparty.game.screens.GameplayScreen.Box;
 import com.blocparty.game.utilities.GameClickListener;
-
 
 public class GameplayScreen implements Screen {
 
@@ -46,7 +47,7 @@ public class GameplayScreen implements Screen {
     
     //public static ArrayList<Box> boxes;
     //row, column
-    public static Box[][] boxes;
+    public static ExpandingTile[][] expandingTiles;
     
     
     //boolean adjusting = false;
@@ -75,11 +76,7 @@ public class GameplayScreen implements Screen {
     	public float circleRadius = 20.0f;
     	public boolean hasCircle = false;
     }
-    
 
-
-    
-    
     private float getGrowthRate() {
     	//totalTime
     	float growthRate = 10.0f + totalTime / 10.0f;
@@ -98,19 +95,15 @@ public class GameplayScreen implements Screen {
     		baseTime = 0;
     	}
     	
-    	float timeRange = 1.0f - (totalTime / 40.0f);;
+    	float timeRange = 1.0f - (totalTime / 40.0f);
     	if (timeRange < 0.5) {
     		timeRange = 0.5f;
     	}
     	
-    	
     	float nextTime = (float) Math.random() * timeRange + baseTime;
     	return nextTime;
     }
-    
-    
-    
-    
+
     private void resetValues() {
     	gameOver = false;
 		gameOverPrompted = false;
@@ -133,42 +126,78 @@ public class GameplayScreen implements Screen {
         makeItFit();
     }
 
+    // This is where we update the model of the tiles and see if the game must end.
+    public void update(float delta){
+
+        updateSpawnTimes(delta);
+        updateScoreLabel();
+
+        for (int c = 0; c < COLUMN_COUNT; c++) {
+            for (int r = 0; r < ROW_COUNT; r++) {
+                if (expandingTiles[r][c].isActive()) {
+                    expandingTiles[r][c].update(delta);
+                }
+                if(expandingTiles[r][c].isLoser()){
+                    gameOver = true;
+                    gameOverPrompt();
+                }
+            }
+        }
+    }
+
+    private void updateSpawnTimes(float delta) {
+        totalTime += Gdx.graphics.getDeltaTime();
+        timeSoFar += Gdx.graphics.getDeltaTime();
+        if (timeSoFar > nextCircleTime) {
+
+            spawnCircle();
+
+            //reset timers
+            timeSoFar = 0;
+            //time is 1.0 -> 2.0s?
+
+            //TODO
+            //nextCircleTime = (float) Math.random() * 1.0f + 1.0f;
+            nextCircleTime = generateNextSpawnTime();
+        }
+    }
+
+    public static void updateScoreLabel() {
+        scoreLabel.setText("Score: " + score);
+    }
+
+    private void spawnCircle() {
+        int column = (int) (Math.random() * 4);
+        int row = (int) (Math.random() * 2);
+
+        if (!expandingTiles[row][column].isActive()) {
+            expandingTiles[row][column].activateTile(10);
+        }
+    }
+
     @Override
-    public void render(float arg0) {
+    public void render(float delta) {
+
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        
-        
-        
+
         drawRectangles();
         
         drawCircles();
         
-        
-        float delta = Gdx.graphics.getDeltaTime();
-        
-        //updateTimes(delta);
-        
         if (!gameOver) {
         	updateSpawnTimes(delta);
-        	growCircles(delta);
+            update(delta);
         }
-        
-        
-        checkCircleSize();
-        
         
         if (gameOver && !gameOverPrompted) {
         	gameOverPrompt();
         }
-        
-        
-		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-		stage.draw();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
-    
-    
+
     private void drawRectangles() {
     	shapeBatch.begin(ShapeType.Line);
         shapeBatch.setColor(Color.BLACK);
@@ -188,78 +217,13 @@ public class GameplayScreen implements Screen {
         
         for (int c = 0; c < COLUMN_COUNT; c++) {
         	for (int r = 0; r < ROW_COUNT; r++) {
-        		if (boxes[r][c].hasCircle) {
-        			int x = c*boxWidth + boxWidth/2;
-        			int y = r*boxHeight + boxHeight/2;
-        			
-        			shapeBatch.circle(x, y, boxes[r][c].circleRadius);
+        		if (expandingTiles[r][c].isActive()) {
+                    expandingTiles[r][c].draw(shapeBatch);
         		}
         	}
         }
         
         shapeBatch.end();
-    }
-    
-    private void updateSpawnTimes(float delta) {
-        totalTime += Gdx.graphics.getDeltaTime();
-        timeSoFar += Gdx.graphics.getDeltaTime();
-        if (timeSoFar > nextCircleTime) {
-        	//spawn the new circle
-        	//System.out.println("spawn");
-        	spawnCircle();
-        	
-        	
-        	
-        	//reset timers
-        	timeSoFar = 0;
-        	//time is 1.0 -> 2.0s?
-        	
-        	//TODO
-        	//nextCircleTime = (float) Math.random() * 1.0f + 1.0f;
-        	nextCircleTime = generateNextSpawnTime();
-        }
-    }
-    
-    private void growCircles(float delta) {
-    	for (Box[] boxRC : GameplayScreen.boxes) {
-    		for (Box b : boxRC) {
-        		
-    			if (b.hasCircle) {
-    				//b.circleRadius = b.circleRadius + delta * 10.0f;
-    				b.circleRadius = b.circleRadius + delta * getGrowthRate();
-    			}
-    			
-    		}
-    	}
-    }
-    
-    
-    private void spawnCircle() {
-    	int column = (int) (Math.random() * 4);
-    	int row = (int) (Math.random() * 2);
-    	
-    	if (!boxes[row][column].hasCircle) {
-    		boxes[row][column].hasCircle = true;
-        	boxes[row][column].circleRadius = 30;
-    	}
-    }
-    
-    
-    public static void updateScoreLabel() {
-    	scoreLabel.setText("Score: " + score);
-    }
-    
-    
-    private void checkCircleSize() {
-    	for (Box[] boxRC : GameplayScreen.boxes) {
-    		for (Box b : boxRC) {
-    			if (b.hasCircle) {
-    				if (b.circleRadius > boxWidth / 2) {
-    					gameOver = true;
-    				}
-    			}
-    		}
-    	}
     }
     
     private void gameOverPrompt() {
@@ -279,15 +243,8 @@ public class GameplayScreen implements Screen {
 		prompt.show(stage);
 		prompt.setSize(200f, 100f);
     }
-    
-    
-    
+
     private void makeItFit() {
-
-
-        
-    	
-    	
     	float scaleW = Gdx.graphics.getWidth() / INTENDED_WIDTH;
     	float scaleH = Gdx.graphics.getHeight() / INTENDED_HEIGHT;
     	
@@ -299,58 +256,75 @@ public class GameplayScreen implements Screen {
     		minScale = scaleH;
     		maxScale = scaleW;
     	}
-    	
-    	
-    	
-    	
+
     	batch = new SpriteBatch();
         shapeBatch = new ShapeRenderer();
 
         //hud = GameScreenHUD.create(round);
-
 
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
 
         boxWidth = width / COLUMN_COUNT;
         boxHeight = height / ROW_COUNT;
-        
-        
-        
-        
-        //boxes = new ArrayList<Box>();
-        boxes = new Box[2][4];
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+//        stage.addListener(new InputListener(){
+//            @Override
+//            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+//                for (int i = 0; i < COLUMN_COUNT; i++) {
+//                    for (int j = 0; j < ROW_COUNT; j++) {
+//                        if(expandingTiles[i][j].equals(stage.hit(x, y, false))){
+//                            return true;
+//                        }
+//                    }
+//                }
+//
+//                Gdx.app.exit();
+//                return true;
+//            }
+//        });
+
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+
+        expandingTiles = new ExpandingTile[2][4];
+
         for (int i = 0; i < COLUMN_COUNT; i++) {
         	for (int j = 0; j < ROW_COUNT; j++) {
-        		Box b = new Box();
-        		b.row = j;
-        		b.column = i;
-        		//boxes.add(b);
-        		boxes[j][i] = b;
+        		ExpandingTile tile = new ExpandingTile(boxWidth, boxHeight);
+                tile.setPosition(i * boxWidth + boxWidth / 2, j * boxHeight + boxHeight / 2);
+        		expandingTiles[j][i] = tile;
+                stage.addActor(tile);
         	}
         }
         
-        
+//        InputMultiplexer multiplexer = new InputMultiplexer();
+//        multiplexer.addProcessor(stage);
+//        multiplexer.addProcessor(new InputAdapter(){
+//             @Override
+//             public boolean touchDown (int x, int y, int pointer, int button) {
+//                 for (int i = 0; i < COLUMN_COUNT; i++) {
+//                     for (int j = 0; j < ROW_COUNT; j++) {
+//                         if (expandingTiles[i][j].equals(stage.hit(x, y, false))) {
+//                             return true;
+//                         }
+//                     }
+//                 }
+//
+//                 Gdx.app.exit();
+//                 return true;
+//             }
+//         });
 
-        
-        stage = new Stage();
-        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
-        
-        InputMultiplexer multiplexer = new InputMultiplexer();
-        multiplexer.addProcessor(stage);
-        multiplexer.addProcessor(new GameClickListener());
-        Gdx.input.setInputProcessor(multiplexer);
-        
+//        Gdx.input.setInputProcessor(multiplexer);
+        Gdx.input.setInputProcessor(stage);
         
         scoreLabel = new Label("Score: 0", skin);
         scoreLabel.setPosition(20, height - 40);
         //score.setPosition(20, 20);
         stage.addActor(scoreLabel);
-        
-        
     }
-
-
 
     //===========================================
 
--- core/src/com/blocparty/game/utilities/GameClickListener.java
+++ core/src/com/blocparty/game/utilities/GameClickListener.java
@@ -19,48 +19,48 @@ public class GameClickListener extends InputAdapter {
 
 
     @Override
-    public boolean touchUp (int x, int y, int pointer, int button) {
+    public boolean touchDown (int x, int y, int pointer, int button) {
 
         
-    	y = Gdx.graphics.getHeight() - y;
-    	
-    	
-    	//System.out.println("up");
-        
-    	
-    	//int radius = GameplayScreen.boxWidth / 2;
-    	
-    	boolean hitCircle = false;
-    	
-    	//this can be made more efficient with mod
-    	for (Box[] boxRC : GameplayScreen.boxes) {
-    		for (Box b : boxRC) {
-    			int centerX = b.column * GameplayScreen.boxWidth + GameplayScreen.boxWidth / 2;
-        		int centerY = b.row * GameplayScreen.boxHeight + GameplayScreen.boxHeight / 2;
-        		float radius = b.circleRadius;
-        		
-        		if ((x-centerX)*(x-centerX) + (y-centerY)*(y-centerY) < radius*radius) {
-        			//System.out.println(b.row);
-        			//System.out.println(b.column);
-        			
-        			//clicked a circle
-        			if (b.hasCircle && !GameplayScreen.gameOver) {
-        				b.hasCircle = false;
-        				
-        				GameplayScreen.score++;
-        				GameplayScreen.updateScoreLabel();
-        				
-        				hitCircle = true;
-        				
-        				//System.out.println(GameplayScreen.score);
-        			}
-        		}
-    		}
-    	}
-    	
-        if (!hitCircle) {
-        	GameplayScreen.gameOver = true;
-        }
+//    	y = Gdx.graphics.getHeight() - y;
+//
+//
+//    	//System.out.println("up");
+//
+//
+//    	//int radius = GameplayScreen.boxWidth / 2;
+//
+//    	boolean hitCircle = false;
+//
+//    	//this can be made more efficient with mod
+//    	for (Box[] boxRC : GameplayScreen.boxes) {
+//    		for (Box b : boxRC) {
+//    			int centerX = b.column * GameplayScreen.boxWidth + GameplayScreen.boxWidth / 2;
+//        		int centerY = b.row * GameplayScreen.boxHeight + GameplayScreen.boxHeight / 2;
+//        		float radius = b.circleRadius;
+//
+//        		if ((x-centerX)*(x-centerX) + (y-centerY)*(y-centerY) < radius*radius) {
+//        			//System.out.println(b.row);
+//        			//System.out.println(b.column);
+//
+//        			//clicked a circle
+//        			if (b.hasCircle && !GameplayScreen.gameOver) {
+//        				b.hasCircle = false;
+//
+//        				GameplayScreen.score++;
+//        				GameplayScreen.updateScoreLabel();
+//
+//        				hitCircle = true;
+//
+//        				//System.out.println(GameplayScreen.score);
+//        			}
+//        		}
+//    		}
+//    	}
+//
+//        if (!hitCircle) {
+//        	GameplayScreen.gameOver = true;
+//        }
 
         return false;
     }
